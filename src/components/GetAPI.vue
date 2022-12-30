a346cbe653e204f841e889a1d57e6572ba114aef
Junki <apple@Apples-MacBook-Air.local>
2022-12-30 19:48:24 +0900

    #6 CSSをあてた

--- src/components/GetAPI.vue
+++ src/components/GetAPI.vue
@@ -4,8 +4,18 @@ const userData = await userDataController();
 </script>
 
 <template>
-  <div v-for="(user, key) in userData" :key="key">
+  <div v-for="(user, key) in userData" :key="key" className="user">
     <div>UserID: {{ user[0] }}</div>
     <div>Title: {{ user[1] }}</div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.user {
+  text-align: left;
+  width: 500px;
+  background-color: rgb(188, 255, 255);
+  margin: auto;
+  padding: 2rem;
+}
+</style>
