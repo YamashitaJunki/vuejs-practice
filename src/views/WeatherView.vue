f0f4f72c58156a1c701f89b26c3eca994d11f2d2
Junki <apple@Apples-MacBook-Air.local>
2022-12-31 11:19:18 +0900

    #8 天気予報表示コンポーネント作成

---
+++ src/views/WeatherView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import WeatherForecast from '@/components/WeatherForecast.vue';
+</script>
+
+<template>
+  <suspense>
+    <div><WeatherForecast /></div>
+  </suspense>
+</template>
